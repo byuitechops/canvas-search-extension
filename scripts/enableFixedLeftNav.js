2faca1b6d9c8dfb075226ac1669968a8b449a759
Muller <mando0975@byui.edu>
2018-06-26 14:15:04 -0600

    check for global nav expansion

--- scripts/enableFixedLeftNav.js
+++ scripts/enableFixedLeftNav.js
@@ -1,10 +1,29 @@
 function enableFixedLeftNav() {
-  var leftNavStyle = document.getElementById('left-side').style;
-  leftNavStyle.position = "fixed";
-  leftNavStyle.display = "block";
-  leftNavStyle.top = "70px";
-  leftNavStyle.left = "55px";
+    var body = document.getElementsByTagName('body')[0];
+    var leftNavStyle = document.getElementById('left-side').style;
+    if(body.classList.contains('primary-nav-expanded')) {
+      console.log('open');
+      setOpenGlobalStyles(leftNavStyle);
+    }
+    else {
+        setClosedGlobalStyles(leftNavStyle);
+    }
 }
+
+function setClosedGlobalStyles(elStyle) {
+    elStyle.position = "fixed";
+    elStyle.display = "block";
+    elStyle.top = "70px";
+    elStyle.left = "55px";
+}
+
+function setOpenGlobalStyles(elStyle) {
+    elStyle.position = "fixed";
+    elStyle.display = "block";
+    elStyle.top = "70px";
+    elStyle.left = "70px";
+}
+
 
 chrome.storage.sync.get({
   fixedLeftNav: false,
